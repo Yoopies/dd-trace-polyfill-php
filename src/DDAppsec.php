a0980e394cfe0289eb47933f6468c7e101168297
AntoineLemaire <antoine@bouffe.io>
2023-04-12 15:27:49 +0200

    Split method track_user_login_event

--- src/DDAppsec.php
+++ src/DDAppsec.php
@@ -4,7 +4,11 @@ namespace datadog\appsec;
 
 if (!extension_loaded('ddappsec')) {
 
-    function track_user_login_event($userId, $success, $metadata)
+    function track_user_login_success_event($userId, $metadata)
+    {
+    }
+
+    function track_user_login_failure_event($userId, $exist, $metadata)
     {
     }
 
